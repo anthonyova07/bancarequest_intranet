f79be02481496fcff8f892066589dcb0492704cf
Anthony <anthonyovalles@gmail.com>
2017-12-20 14:46:18 -0400

    se le agrego estilo al panel de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<div class="panel panel-default">
+<div class="panel panel-primary" style="box-shadow: 4px 3px 7px 1px black;">
     <div class="panel-heading">Bienvenido - Sistema de gestion de requerimientos tecnologicos Bancamerica</div>
 
     <div class="panel-body">
-        <p>Hola</p>
+        <p>Haga click en el link de login de la parte superior para introducir sus credenciales de usuario</p>
     </div>
 </div>
   
